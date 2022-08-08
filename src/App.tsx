fa22abf6eb1a0ee2615f7cfd32129c4daa8b19ff
unknown <truongduchuy910@gmail.com>
2022-08-08 14:47:05 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <section>
+        <h1>Trần Ngọc Huy</h1>
+        <code>@truongduchuy910</code>
+        <hr />
+        <div>
+          <a href="https://www.linkedin.com/in/truongduchuy910/">linkedin</a>
+          <a href="https://twitter.com/truongduchuy910">twitter</a>
+          <a href="https://github.com/truongduchuy910">github</a>
+        </div>
+        <a href="mailto:truongduchuy910@gmail.com">truongduchuy910@gmail.com</a>
+      </section>
+    </main>
+  )
+}
+
+export default App
